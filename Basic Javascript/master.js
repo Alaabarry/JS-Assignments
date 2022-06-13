889f0cab70a85623c6baf8b0f70eb5948a758aa8
unknown <zerohand99@gmail.com>
2022-06-13 20:26:51 +0300

    Js file

--- Basic Javascript/master.js
+++ Basic Javascript/master.js
@@ -1,1 +1,12 @@
-alert("Hi,World");
+
+
+var str1="We"; //Assign variable to string
+var str2="Love"; //Assign variable to string
+var str3="JavaScript";  //Assign variable to string
+ 
+var color1=str1.fontcolor("blue");  //using fontcolor method to variable str1
+var color2=str2.fontcolor("red");  //using  fontcolor method to variable str2
+var color3=str3.fontcolor("purple"); //using fontcolor method to variable str3
+document.write(color1.fontsize(7) +color2.fontsize(7)+color3.fontsize(7)) ; //concatenate the variables together and apply fontsize method to variables
+
+window.alert(str1 +str2+ str3);//alert concatenate variables
