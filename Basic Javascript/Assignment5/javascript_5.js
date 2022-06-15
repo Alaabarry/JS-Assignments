105edb685fafcf43732f6aca226c746c42b30858
unknown <zerohand99@gmail.com>
2022-06-15 20:16:02 +0300

    Assignment5

---
+++ Basic Javascript/Assignment5/javascript_5.js
@@ -0,0 +1,33 @@
+document.write(3E319 + "<br>");  // Postive infinity
+document.write(-3E339 + "<br>");  //negative infinity
+document.write( 10 < 9 ); // boolean logic false 
+document.write("<br>")  //line break
+document.write( 100 > 80);//boolean logic true 
+console.log(10 +5 -3); // perform math operation on browser console 
+document.write("<br>")  //line break 
+document.write("five"+ 5); //type coercion 
+console.log(5>10); // boolean logic in console 
+document.write("<br>")  //line break  
+document.write( 10 == 10);// comparison with double equal true   
+document.write("<br>")
+document.write(8 == 6);// comparison with douvle equal false  
+document.write("<br>")  //line break  
+ var x = 5; 
+ var y ="5"; 
+ document.write(x === y); //comparison with triple equal return false  same value different dataType. 
+ document.write("<br>")  //line break   
+ var a = 10; 
+ var b= 10; 
+ document.write( a === b); // comparison with triple equal return true same value same dataType.  
+ document.write("<br>")  //line break  
+ var z = 4; 
+ var x = "four" 
+ document.write( z ===x ); // comparison with triple equal return false  different values , different dataType.  
+ document.write("<br>")  //line break 
+ document.write( 10 > 9 && 5 < 10); //logic comparsion return true.   
+ document.write("<br>")  //line break  
+ document.write( 100< 90 ||60 < 40);//logic comparsion return false.   
+ document.write("<br>")  //line break  
+ document.write(!(5>4)); // Not operater return false. 
+ document.write("<br>")  //line break   
+ document.write(!(10<9)); //not operater return true.
